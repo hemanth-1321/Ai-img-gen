8cc17e215f9c93c8e93beed8684d90a1c245bb21
hemanth-1321 <hemanth02135@gmail.com>
2025-03-01 21:10:40 +0530

    typesErr

--- apps/server/src/models/FalAiModel.ts
+++ apps/server/src/models/FalAiModel.ts
@@ -27,17 +27,17 @@ export class FalAiModel {
 
   public async TrainModel(zipUrl: string, triggeredWord: string) {
     console.log("ZipUrl", zipUrl, triggeredWord);
-    const { request_id, response_url } = await fal.queue.submit(
-      "fal-ai/flux-lora-fast-training",
-      {
-        input: {
-          images_data_url: zipUrl,
-          trigger_word: triggeredWord,
-        },
-        webhookUrl: `${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook/train`,
-      }
-    );
-    return { request_id, response_url };
+    // const { request_id, response_url } = await fal.queue.submit(
+    //   "fal-ai/flux-lora-fast-training",
+    //   {
+    //     input: {
+    //       images_data_url: zipUrl,
+    //       trigger_word: triggeredWord,
+    //     },
+    //     webhookUrl: `${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook/train`,
+    //   }
+    // );
+    return { request_id: "", response_url: "" };
   }
 
   public async generateImageSync(tensorPath: string) {
