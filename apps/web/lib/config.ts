731398c2d321e82d5b1162306adcfbdc26f5f7b5
hemanth <hemanth02135@gmail.com>
2025-05-05 14:01:25 +0530

    build

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://photogenai.hemanth.buzz";
 export const AWS_S3_URL = "https://fal-model.s3.ap-south-1.amazonaws.com";
